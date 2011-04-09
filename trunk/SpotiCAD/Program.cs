c9e3669c3cf9b80204f2bde7f3081633459ccd12
KodessR <KodessR@18cb9d19-c8e9-a75a-2d49-38d8d08fb3ce>
2011-04-09 20:48:08 +0000

    First Shift

--- trunk/SpotiCAD/Program.cs
+++ trunk/SpotiCAD/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace SpotiCAD
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Text;
+
+/*
+ * This is NON-FUNCTIONING code.
+ * The only way to exit is to kill the process.
+ * */
+namespace SpotiCAD
 {
     class Program
     {
@@ -10,16 +19,46 @@
 
     class SpotiCAD
     {
+
+        private IntPtr SpotifyHandle;
+        private String wTitle;
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText", CharSet = CharSet.Ansi)]
+        public static extern bool GetWindowText(IntPtr hWnd, [OutAttribute()] StringBuilder strNewWindowName, Int32 maxCharCount);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowTextLength", CharSet = CharSet.Ansi)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
         //Constructor
         public SpotiCAD()
         {
-
+            this.SpotifyHandle = IntPtr.Zero;
+            //Start our inf loop
+            while (true)
+            {
+            }
         }
 
-        //Using user32.dll we grab the spotify window
-        private void getWindow()
+        //Get the Handle from the Process
+        private void getHandle()
         {
+            Process[] sProcessArr = Process.GetProcessesByName("spotify");
+            //Check if the Spotify Process is running
+            if (sProcessArr.Length > 0)
+            {
+                Process sProcess = sProcessArr[0];
+                //Dump the Handle
+                this.SpotifyHandle = sProcess.MainWindowHandle;
+            }
+        }
 
+        //Get the title of the spotify window and store it.
+        private void getWindowTitle()
+        {
+            int length = GetWindowTextLength(SpotifyHandle);
+            StringBuilder sb = new StringBuilder(length + 1);
+            GetWindowText(SpotifyHandle, sb, sb.Capacity);
+            wTitle = sb.ToString();
         }
 
         //We grab the title of the spotify window and strip out the track name.
